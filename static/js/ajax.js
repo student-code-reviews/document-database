ad57890d51f07cb43420086434a7c56e22f80613
AwksJen <awksjen@gmail.com>
2020-03-14 23:32:31 -0500

    add ajax.js file and one on change event

---
+++ static/js/ajax.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+
+function showFortune(evt) {
+
+    
+    $.get('/fortune', (response) => {
+      $('#fortune-text').text(response);
+    });
+}
+
+$('#get-fortune-button').on('click', showFortune);
+//show a quote by an author in drop down options
+
+function showLewisQuote(evt) {
+
+    $.get('/author_choice', (response) => {
+        $('#quote-text').text(response);
+    });
+}
+
+$('#davy-lewis').on('change', showLewisQuote);
