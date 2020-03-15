a4e410b9188322f0ea0f6339b3b8f7e5c43b27fd
AwksJen <awksjen@gmail.com>
2020-03-14 23:31:27 -0500

    add showLewisQuote route

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 # from models import everything
 import jinja2
+import random
 
 
 app = Flask(__name__)
@@ -9,8 +10,19 @@ app = Flask(__name__)
 app.jinja_env.undefined = jinja2.StrictUndefined
 
 
+QUOTES = ["a i am ", "b i am ", "sam i am "]
+
+
 @app.route('/')
 def index():
     """Show the index."""
 
     return render_template('index.html')
+
+
+@app.route('/author_choice')
+def one_random_quote():
+    """Return a single quote as a text string"""
+
+#return a random quote
+    return random.choice(QUOTES)
