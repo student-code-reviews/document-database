dda5fc7c6e3ef22a5e366aa3caca748fbda76d15
AwksJen <awksjen@gmail.com>
2020-03-26 19:43:28 -0500

    correct javascript ad jquery syntax so it works

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,24 +1,12 @@
 "use strict";
 
-
-// PART 1: SHOW A FORTUNE
-
-// function showFortune(evt) {
-
-    
-//     $.get('/fortune', (response) => {
-//       $('#fortune-text').text(response);
-//     });
-// }
-
-// $('#get-fortune-button').on('click', showFortune);
-// //show a quote by an author in drop down options
-
-function showLewisQuote(evt) {
-
-    $.get('/', (response) => {
-        $('#quote-text').text(response);
-    });
+function replaceRandomQuote(results) {
+    $("#random-quote-text").html(results);
 }
 
-$('#davy-lewis').on('change', showLewisQuote);
+function showRandomQuote(evt) {
+    $.get('/random_quote', replaceRandomQuote);
+}
+
+$('#get-random-quote-button').on('click', showRandomQuote);
+
