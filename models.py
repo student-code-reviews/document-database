d3d48779c00415823e1be64494d9b65baf831101
AwksJen <awksjen@gmail.com>
2020-03-26 19:40:33 -0500

    got rid of git head matter

--- models.py
+++ models.py
@@ -17,11 +17,8 @@ class Author(db.Model):
     author_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     quotes = db.relationship('Quote', backref='author')
-<<<<<<< HEAD
-    #sources = db.relationship('Quote', secondary='sources')
-=======
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
 
+    #sources = db.relationship('Quote', secondary='sources'
     def __repr__(self):
         return f'<Author {self.id} | {self.name}>'
 
@@ -33,16 +30,11 @@ class Quote(db.Model):
     quote_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     aquote = db.Column(db.String(), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'))
-<<<<<<< HEAD
     #sources = db.relationship('Author', secondary='sources')
-=======
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
-
     def __repr__(self):
         return f'<Quote {self.id} | {self.aquote} >'
 
 
-<<<<<<< HEAD
 # class Sources(db.Model):
 
 #     __tablename__ = 'sources'
@@ -51,10 +43,6 @@ class Quote(db.Model):
 #     quote_id = db.Column(db.Integer, db.ForeignKey('quotes.quote_id'))
 #     source = db.Column(db.String())
 #     source_id = db.Column(db.Integer, primary_key=True)
-
-
-=======
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
 def connect_to_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///database'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -65,19 +53,15 @@ def connect_to_db(app):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
+
     from flask import Flask
-=======
+
     from flask import Flask 
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
+
     app = Flask(__name__)
 
     connect_to_db(app)
     db.drop_all()
     db.create_all()
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> ec52212b70e2697f830d74f530f17c01174749eb
     print('Connected to database, tables ready.')
